2b7ee1f1dcf9cc678352f914152d02cbbd19cbbf
kembo <asininefatuity@gmail.com>
2025-09-17 20:45:37 +0300

    finetuned my class

--- core/utils/auth.py
+++ core/utils/auth.py
@@ -1,27 +1,28 @@
 from jose import jwt, JWTError
 import logging
 import requests
-from typing import Dict, Any, Union
+from typing import Dict, Any
 from decouple import config
 from django.core.cache import cache
 
 
 class VerifyOidcToken:
     OIDC_PUBLIC_KEY_CACHE_KEY = "oidc_jwks"
+    OIDC_USERINFO_CACHE_KEY = "oidc_userinfo"
 
-    def __init__(self, access_token: str):
-        self.access_token = access_token
-        self.oidc_domain = config("OIDC_DOMAIN")
-        self.oidc_audience = config("OIDC_AUDIENCE")
-        self.jwks = self._get_oidc_json_web_key_set()
-        self.decoded_data = self._decode_user_data_from_token()
+    def __init__(self, oidc_token: str):
+        self._oidc_access_token = oidc_token
+        self._oidc_domain = config("OIDC_DOMAIN")
+        self._oidc_audience = config("OIDC_AUDIENCE")
+        self._jwks = self._get_oidc_json_web_key_set()
+        self._decoded_data = self._decode_user_data_from_token()
 
-    def validate_token(self) -> Union[None, Dict[str, Any]]:
+    def validate_token(self) -> Dict[str, Any]:
         user_data = self._get_user_info_from_decoded_claims()
         return user_data
 
     def _get_oidc_json_web_key_set(self):
-        jwks_url = f"https://{self.oidc_domain}/.well-known/jwks.json"
+        jwks_url = f"https://{self._oidc_domain}/.well-known/jwks.json"
         jwks = cache.get(self.OIDC_PUBLIC_KEY_CACHE_KEY)
         if not jwks:
             jwks = requests.get(jwks_url).json()
@@ -31,10 +32,24 @@ class VerifyOidcToken:
             cache.set(self.OIDC_PUBLIC_KEY_CACHE_KEY, jwks, timeout=60 * 60)
         return jwks
 
+    def _get_user_info_endpoint(self):
+        well_known_url = f"https://{self._oidc_domain}/.well-known/openid-configuration"
+        info_response = cache.get(self.OIDC_USERINFO_CACHE_KEY)
+        if not info_response:
+            response = requests.get(well_known_url)
+            if response.status_code == 200:
+                config_data = response.json()
+                info_response = config_data.get("userinfo_endpoint")
+                cache.set(self.OIDC_USERINFO_CACHE_KEY, info_response, timeout=60 * 60)
+            else:
+                logging.error(f"{__name__}: Unable to fetch OIDC configuration")
+        return info_response
+
     def _decode_user_data_from_token(self):
-        unverified_header = jwt.get_unverified_header(self.access_token)
+        unverified_header = jwt.get_unverified_header(self._oidc_access_token)
         rsa_key = {}
-        for key in self.jwks["keys"]:
+        decoded_data = {}
+        for key in self._jwks["keys"]:
             if key["kid"] == unverified_header["kid"]:
                 rsa_key = {
                     "kty": key["kty"],
@@ -48,29 +63,39 @@ class VerifyOidcToken:
             logging.error(
                 f"{__name__}: Unable to find OIDC provider public key to verify token"
             )
-            return None
+            return decoded_data
         try:
-            decoded = jwt.decode(
-                self.access_token,
+            decoded_data = jwt.decode(
+                self._oidc_access_token,
                 rsa_key,
                 algorithms=["RS256"],
-                audience=self.oidc_audience,
-                issuer=f"https://{self.oidc_domain}".rstrip("/"),
+                audience=self._oidc_audience,
+                issuer=f"https://{self._oidc_domain}".rstrip("/"),
             )
-            return decoded
         except JWTError as e:
             logging.error(f"{__name__}: Unable to decode token - {str(e)}")
-        return None
+        return decoded_data
+
+    def _request_user_info_from_oidc_provider(self):
+        userinfo_url = self._get_user_info_endpoint()
+        user_info = {}
+        if not userinfo_url:
+            return user_info
+        headers = {"Authorization": f"Bearer {self._oidc_access_token}"}
+        response = requests.get(userinfo_url, headers=headers)
+        if response.status_code == 200:
+            user_info = response.json()
+        return user_info
 
     def _get_user_info_from_decoded_claims(self):
         """
-        NOTE: sample decoded data looks like this
+        NOTE: sample decoded data looks like this (Auth0 OIDC provider example)
         {
-            "iss": "https://dev-ayadil08ocxycfzg.us.auth0.com/",
+            "iss": "https://dev-myown-domain.us.auth0.com/",
             "sub": "google-oauth2|116335214551149049094",
             "aud": [
-                "https://dev-ayadil08ocxycfzg.us.auth0.com/api/v2/",
-                "https://dev-ayadil08ocxycfzg.us.auth0.com/userinfo"
+                "https://dev-myown-domain.us.auth0.com/api/v2/",
+                "https://dev-myown-domain.us.auth0.com/userinfo"
             ],
             "iat": 1742339743,
             "exp": 1742426143,
@@ -78,13 +103,13 @@ class VerifyOidcToken:
             "azp": "7muEEQsA8kcvlzgZHbLGcSYTEzFDKWdN",
         }
         """
-        if not self.decoded_data:
-            return None
+        if not self._decoded_data:
+            return {}
+        user_info = self._request_user_info_from_oidc_provider()
         return {
-            "oidc_id": self.decoded_data.get("sub"),
-            "oidc_issuer": self.decoded_data.get("iss"),
-            "email": self.decoded_data.get("email"),
-            "first_name": self.decoded_data.get("given_name"),
-            "last_name": self.decoded_data.get("family_name"),
-            "avatar": self.decoded_data.get("picture"),
+            "oidc_subject": self._decoded_data.get("sub"),
+            "oidc_issuer": self._decoded_data.get("iss"),
+            "email": user_info.get("email"),
+            "first_name": user_info.get("given_name", "Anon"),
+            "last_name": user_info.get("family_name", "User"),
         }

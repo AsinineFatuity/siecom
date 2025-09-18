07eed44814fb8bbb3a3110a77df6097e365256a8
kembo <asininefatuity@gmail.com>
2025-09-18 09:49:04 +0300

    aaded useful comment

--- core/graphql/product/mutation.py
+++ core/graphql/product/mutation.py
@@ -10,6 +10,11 @@ from siecom.decorators import logged_in_user_required
 
 
 class CreateProduct(graphene.Mutation):
+    """
+    NOTE: Assumes that uploaded products have same category
+    such that the category is a list of the hierarchy with the last item being the most specific.
+    """
+
     class Arguments:
         products = graphene.List(ProductInputType, required=True)
         categories = graphene.List(CategoryInputType, required=True)

1b3b20ee8133dbf1b4bbf0a021414bb62c26062b
kembo <asininefatuity@gmail.com>
2025-09-18 08:43:12 +0300

    created a middleware

--- siecom/middleware/__init__.py
+++ siecom/middleware/__init__.py
@@ -1,3 +1,4 @@
 from siecom.middleware.introspection import GrapheneBlockIntrospectionMiddleware
+from siecom.middleware.auth import OIDCAuthenticationMiddleware
 
-__all__ = ["GrapheneBlockIntrospectionMiddleware"]
+__all__ = ["GrapheneBlockIntrospectionMiddleware", "OIDCAuthenticationMiddleware"]

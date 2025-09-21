a2b0648dc3ff9f8cb185aeb22284ee79eb3eeaeb
kembo <asininefatuity@gmail.com>
2025-09-21 12:18:35 +0300

    prod files

--- docker/start.sh
+++ docker/start.sh
@@ -6,4 +6,7 @@ set -o nounset
 # Apply database migrations and start the server
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn siecom.wsgi:application --bind 0.0.0.0:8000 --reload
+if [ "$ENVIRONMENT" = "production" ]; then
+    gunicorn siecom.wsgi:application --bind 0.0.0.0:8000
+else
+    gunicorn siecom.wsgi:application --bind 0.0.0.0:8000 --reload

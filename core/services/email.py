c9ce8cfda440fad25b35167cc0214989b7a46d80
kembo <asininefatuity@gmail.com>
2025-09-22 21:11:40 +0300

    fixed github actions fail because of variables

--- core/services/email.py
+++ core/services/email.py
@@ -5,38 +5,37 @@ from typing import List
 
 
 class EmailService:
-    ZEPTO_AUTH_TOKEN = config("ZEPTO_AUTH_TOKEN", default="", cast=str)
-    ZEPTO_URL = "https://api.zeptomail.com/v1.1/email"
-    DEFAULT_MAIL_FROM = config("EMAIL_HOST_USER", default="", cast=str)
-
     def __init__(
         self,
         recipients: List[str],
         html_body: str,
     ):
-        self.recipients = recipients
-        self.subject = "Order Confirmation - Siecom Stores"
-        self.html_body = html_body
+        self._recipients = recipients
+        self._subject = "Order Confirmation - Siecom Stores"
+        self._html_body = html_body
+        self._zepto_auth_token = config("ZEPTO_AUTH_TOKEN", default="", cast=str)
+        self._zepto_url = "https://api.zeptomail.com/v1.1/email"
+        self._default_mail_from = config("EMAIL_HOST_USER", default="", cast=str)
 
     def send_emails(self):
         headers = {
-            "Authorization": self.ZEPTO_AUTH_TOKEN,
+            "Authorization": self._zepto_auth_token,
             "Accept": "application/json",
             "Content-Type": "application/json",
         }
         payload = {
-            "from": {"address": self.DEFAULT_MAIL_FROM, "name": "Siecom Stores"},
+            "from": {"address": self._default_mail_from, "name": "Siecom Stores"},
             "to": [
                 {"email_address": {"address": recipient}}
-                for recipient in self.recipients
+                for recipient in self._recipients
             ],
-            "subject": self.subject,
-            "htmlbody": self.html_body,
+            "subject": self._subject,
+            "htmlbody": self._html_body,
             "attachments": [],
             "track_opens": True,
             "track_clicks": True,
         }
-        response = requests.post(self.ZEPTO_URL, headers=headers, json=payload)
+        response = requests.post(self._zepto_url, headers=headers, json=payload)
         logging.info(
             f"{__name__}: Zepto response: status code{response.status_code}, text \n - {response.text}"
         )
--- core/services/sms.py
+++ core/services/sms.py
@@ -6,37 +6,36 @@ from decouple import config
 
 
 class SMSService:
-    AT_API_KEY = config("AT_API_KEY")
-    AT_USERNAME = config("AT_USERNAME")
-
     def __init__(self, recipients: list[str], message: str):
-        self.recipients = recipients
-        self.message = message
-        self.sms_service = None
+        self._recipients = recipients
+        self._message = message
+        self._sms_service = None
+        self._at_api_key = config("AT_API_KEY")
+        self._at_username = config("AT_USERNAME")
         self._init_service()
         self._format_phone_numbers()
         self._response = {"message": message, "recipients": []}
 
     def _init_service(self):
         try:
-            africastalking.initialize(self.AT_USERNAME, self.AT_API_KEY)
-            self.sms_service = africastalking.SMS
+            africastalking.initialize(self._at_username, self._at_api_key)
+            self._sms_service = africastalking.SMS
         except Exception as e:
             traceback.print_exc()
             logging.error(f"{__name__}: Failed to initialize SMS service: {e}")
 
     def _format_phone_numbers(self):
-        for i, recipient in enumerate(self.recipients):
+        for i, recipient in enumerate(self._recipients):
             if not recipient.startswith("+"):
-                self.recipients[i] = f"+{recipient}"
-            self.recipients[i] = self.recipients[i].replace(" ", "")
+                self._recipients[i] = f"+{recipient}"
+            self._recipients[i] = self._recipients[i].replace(" ", "")
 
     def send_sms(self):
-        if not self.sms_service:
+        if not self._sms_service:
             logging.error(f"{__name__}: SMS service not initialized.")
             return self._response
         try:
-            response = self.sms_service.send(self.message, self.recipients)
+            response = self._sms_service.send(self._message, self._recipients)
             self._response = self._format_response(response)
         except Exception as e:
             traceback.print_exc()

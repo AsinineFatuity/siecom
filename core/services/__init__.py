8b8b07bf18ee2bf0889e5f4e6d7aa8a13a74b95f
kembo <asininefatuity@gmail.com>
2025-09-22 19:53:51 +0300

    defined background task

--- core/services/__init__.py
+++ core/services/__init__.py
@@ -1,3 +1,5 @@
 from core.services.utils import AlertUtils
 from core.services.email import EmailService
 from core.services.sms import SMSService
+
+__all__ = ["AlertUtils", "EmailService", "SMSService"]

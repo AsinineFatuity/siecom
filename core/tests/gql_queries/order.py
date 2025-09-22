d441cc21c224bcc92e4bc0ccfffa72ba295e43bd
kembo <asininefatuity@gmail.com>
2025-09-22 06:05:21 +0300

    wrote mutation for create order

---
+++ core/tests/gql_queries/order.py
@@ -0,0 +1,32 @@
+def create_order_mutation(product_id: str, quantity: int):
+    return """mutation(address: AddressInputType!) {
+        createOrder(productId: "%s", quantity: %d, address: $address) {
+            order {
+                id
+                product {
+                    id
+                    name
+                    description
+                    price
+                    stock
+                }
+                quantity
+                totalPrice
+                status
+                paymentMethod
+                isPaid
+                address {
+                    id
+                    street
+                    city
+                    phoneNumber
+                }
+                user {
+                    id
+                    email
+                }
+            }
+            success
+            message
+        }
+    }""" % (product_id, quantity)

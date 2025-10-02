c27e7cfd88529c0c5785af7574491d699e08dc67
kembo <asininefatuity@gmail.com>
2025-10-02 08:23:41 +0300

    hacked concurrent cretion of indexes

--- siecom/asgi.py
+++ siecom/asgi.py
@@ -1,9 +1,8 @@
-
-
 import os
 
 from django.core.asgi import get_asgi_application
 from siecom.utils import get_environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'siecom.settings.{get_environment()}')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"siecom.settings.{get_environment()}")
 
 application = get_asgi_application()
--- siecom/utils.py
+++ siecom/utils.py
@@ -2,5 +2,7 @@ from decouple import config
 
 PROD_ENVIRONMENT = "production"
 DEV_ENVIRONMENT = "development"
+
+
 def get_environment():
     return config("ENVIRONMENT", default=PROD_ENVIRONMENT)
